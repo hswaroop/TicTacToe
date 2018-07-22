ec3c63ee04a4965e872cfce7118349bf5fad4c47
kratosjam1994 <himinspace@gmail.com>
2018-07-23 00:45:45 +0530

    player buttons start to disappear.

--- src/pixisprites.ts
+++ src/pixisprites.ts
@@ -1,11 +1,11 @@
-    let type = "WebGL";
+let type = "WebGL";
 
-    if(! PIXI.utils.isWebGLSupported())
-        type = "canvas";
+if(! PIXI.utils.isWebGLSupported())
+    type = "canvas";
 
-    PIXI.utils.sayHello(type);
+PIXI.utils.sayHello(type);
 
-    let Application = PIXI.Application,
+let Application = PIXI.Application,
     Container = PIXI.Container,
     loader = PIXI.loader,
     resources = PIXI.loader.resources,
@@ -13,91 +13,109 @@
     Sprite = PIXI.Sprite,
     Rectangle = PIXI.Rectangle;
 
-    let app = new Application({
-        width: window.innerWidth/2,
-        height: window.innerHeight - 50,
-        antialias: true,
-        transparent: false,
-        resolution: 1
-    });
+let app = new Application({
+    width: window.innerWidth/2,
+    height: window.innerHeight - 50,
+    antialias: true,
+    transparent: false,
+    resolution: 1
+});
 
 //app.renderer.backgroundColor = 0xFFFFFF;
-    document.body.appendChild(app.view);
-    loader.add("../assets/sprites.json");
-    loader.load(setup);
+document.body.appendChild(app.view);
+loader.add("../assets/sprites.json");
+loader.load(setup);
 
-    let TicTacToe, grid, oTurn, xTurn, player1, player2, restart, id;
+let id;
 
-    function setup() {
+function setup() {
 
-        id = PIXI.loader.resources["../assets/sprites.json"].textures;
+    id = PIXI.loader.resources["../assets/sprites.json"].textures;
 
-        class Images {
-            constructor() {
-                this.addGrid();
-                this.addTitle();
-                this.addPlayerSymbols();
-                this.addTurn();
-                this.addRestart();
-            }
+    let grid, TicTacToe, oTurn, xTurn, player1, player2, restart;
 
-            addGrid() {
-                this.grid = new Sprite(id["grid.png"]);
-                app.stage.addChild(this.grid);
-                this.grid.width = app.renderer.width / 2;
-                this.grid.height = app.renderer.height / 2;
-                this.grid.x = app.renderer.width / 2 - this.grid.width / 2;
-                this.grid.y = app.renderer.height / 2 - this.grid.height / 2;
-            }
-            
-            addTitle() {
-                this.TicTacToe = new Sprite(id["TicTacToe.png"]);
-                app.stage.addChild(this.TicTacToe);
-                this.TicTacToe.width = app.renderer.width / 3;
-                this.TicTacToe.height = app.renderer.height / 3;
-                this.TicTacToe.x = this.grid.x;
-            }
+    class Images {
 
-            addPlayerSymbols() {
-                this.oTurn = new Sprite(id["o.png"]);
-                app.stage.addChild(this.oTurn);
-
-                this.xTurn = new Sprite(id["x.png"]);
-                app.stage.addChild(this.xTurn);
-            }
-
-            addTurn() {
-                this.player1 = new Sprite(id["player1.png"]);
-                app.stage.addChild(this.player1);
-                this.player1.width = app.renderer.width / 8;
-                this.player1.height = app.renderer.height / 8;
-                this.player1.x = this.grid.x;
-                this.player1.y = (app.renderer.height - this.grid.y) + this.player1.height / 2;
-                this.player1.interactive = true;
-                this.player1.buttonMode = true;
-
-                this.player2 = new Sprite(id["player2.png"]);
-                app.stage.addChild(this.player2);
-                this.player2.width = app.renderer.width / 8;
-                this.player2.height = app.renderer.height / 8;
-                this.player2.x = (this.grid.x + this.grid.width) - this.player2.width;
-                this.player2.y = (app.renderer.height - this.grid.y) + this.player2.height / 2;
-                this.player2.interactive = true;
-                this.player2.buttonMode = true;
-            }
-
-            addRestart() {
-                this.restart = new Sprite(id["restart.png"]);
-                app.stage.addChild(this.restart);
-                this.restart.width = app.renderer.width / 8;
-                this.restart.height = app.renderer.height / 8;
-                this.restart.x = ((this.player1.x + this.player1.width) +
-                    (this.player2.x - (this.player1.x + this.player1.width)) / 2) - this.restart.width / 2;
-                this.restart.y = (app.renderer.height - this.grid.y) + this.restart.height / 2;
-                this.restart.interactive = true;
-                this.restart.buttonMode = true;
-            }
+        constructor() {
+            this.addGrid();
+            this.addTitle();
+            this.addPlayerSymbols();
+            this.addTurn();
+            this.addRestart();
         }
 
-        let images = new Images();
+        addGrid() {
+            grid = new Sprite(id["grid.png"]);
+            app.stage.addChild(grid);
+            grid.width = app.renderer.width / 2;
+            grid.height = app.renderer.height / 2;
+            grid.x = app.renderer.width / 2 - grid.width / 2;
+            grid.y = app.renderer.height / 2 - grid.height / 2;
+        }
+
+        addTitle() {
+            TicTacToe = new Sprite(id["TicTacToe.png"]);
+            app.stage.addChild(TicTacToe);
+            TicTacToe.width = app.renderer.width / 3;
+            TicTacToe.height = app.renderer.height / 3;
+            TicTacToe.x = grid.x;
+        }
+
+        addPlayerSymbols() {
+            oTurn = new Sprite(id["o.png"]);
+            app.stage.addChild(oTurn);
+
+            xTurn = new Sprite(id["x.png"]);
+            app.stage.addChild(xTurn);
+        }
+
+        addTurn() {
+            player1 = new Sprite(id["player1.png"]);
+            app.stage.addChild(.player1);
+            player1.width = app.renderer.width / 8;
+            player1.height = app.renderer.height / 8;
+            player1.x = grid.x;
+            player1.y = (app.renderer.height - grid.y) + player1.height / 2;
+            player1.interactive = true;
+            player1.buttonMode = true;
+
+            player1.on('mousedown', this.makeChanges);
+
+            player2 = new Sprite(id["player2.png"]);
+            app.stage.addChild(player2);
+            player2.width = app.renderer.width / 8;
+            player2.height = app.renderer.height / 8;
+            player2.x = (grid.x + grid.width) - player2.width;
+            player2.y = (app.renderer.height - grid.y) + player2.height / 2;
+            player2.interactive = true;
+            player2.buttonMode = true;
+            player2.on('mousedown', this.makeChanges);
+        }
+
+        addRestart() {
+            restart = new Sprite(id["restart.png"]);
+            app.stage.addChild(restart);
+            restart.width = app.renderer.width / 8;
+            restart.height = app.renderer.height / 8;
+            restart.x = ((player1.x + player1.width) +
+                (player2.x - (player1.x + player1.width)) / 2) - restart.width / 2;
+            restart.y = (app.renderer.height - grid.y) + restart.height / 2;
+            restart.interactive = true;
+            restart.buttonMode = true;
+        }
+
+        makeChanges()
+        {
+            player1.visible = false;
+            player2.visible = false;
+        }
+
+        /*removePlayerButtons()
+        {
+            this.player1.visible = false;
+            this.player2.visible = false;
+        }*/
     }
+
+    let images = new Images();
+}
